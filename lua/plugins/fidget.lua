d9fe198a33d6abe790ab13c717bae0dd526a5892
kca2250 <2250katok@gmail.com>
2025-05-09 22:17:19 +0900

    fidgetを調整

--- lua/plugins/fidget.lua
+++ lua/plugins/fidget.lua
@@ -1,20 +1,9 @@
 return {
   "j-hui/fidget.nvim",
-  tag = "legacy", -- ※安定版を使いたい場合はこれ、v1ではなくv1以前
-  event = "VeryLazy", -- 遅延読み込み
+  version = "*", -- v1系の最新版を使う
+  event = "VeryLazy",
   config = function()
-    require("fidget").setup({
-      text = {
-        spinner = "dots", -- "dots", "line", "moon", "arc" など種類いろいろ
-      },
-      align = {
-        bottom = true,
-        right = true,
-      },
-      window = {
-        blend = 0,
-      },
-    })
+    require("fidget").setup({})
   end,
 }
 
