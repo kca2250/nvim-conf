fb310933bb210142618fb3672c5448bfa2d3d120
kca2250 <2250katok@gmail.com>
2025-06-06 21:57:10 +0900

    chore: refactor

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,9 +1,10 @@
 -- lua/core/options.lua
 local o = vim.opt
 local g = vim.g
+local a = vim.api
 
 -- Prettierの状態をlualineで使うための初期化
-vim.g.prettier_status = ""
+g.prettier_status = ""
 
 g.mapleader = " "
 g.maplocalleader = " "
@@ -13,26 +14,22 @@ o.relativenumber = false
 o.expandtab = true
 o.shiftwidth = 2
 o.clipboard = "unnamedplus"
-o.wrap = false
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
-vim.opt.hlsearch = true       -- 検索語をハイライト
-vim.opt.incsearch = true      -- インクリメンタルサーチ（入力しながらマッチ）
+
+o.termguicolors = true
+o.cursorline = true
+o.hlsearch = true       -- 検索語をハイライト
+o.incsearch = true      -- インクリメンタルサーチ（入力しながらマッチ）
 
 
 -- vim.api.nvim_set_option_value("winhighlight", "Normal:Normal,NormalNC:Normal", { scope = "local" })
 
 -- ハイライトの設定
-vim.api.nvim_set_hl(0, "ActiveWinSeparator", { fg = "#ff5f87", bold = true }) -- ピンク系
-vim.api.nvim_set_hl(0, "InactiveWinSeparator", { fg = "#5c5c5c" })            -- グレー系
+a.nvim_set_hl(0, "ActiveWinSeparator", { fg = "#ff5f87", bold = true }) -- ピンク系
+a.nvim_set_hl(0, "InactiveWinSeparator", { fg = "#5c5c5c" })            -- グレー系
 
 
 -- Treesitter の型を italic にする
-vim.api.nvim_set_hl(0, "@type", { fg = "#b0b0ff", italic = true })
-vim.api.nvim_set_hl(0, "@type.builtin", { fg = "#9999dd", italic = true })
-vim.api.nvim_set_hl(0, "@property", {
-  underline = true,
-  fg = "#ffcc66", -- 黄色っぽく
-})
+a.nvim_set_hl(0, "@type", { fg = "#b0b0ff", italic = true })
+a.nvim_set_hl(0, "@type.builtin", { fg = "#9999dd", italic = true })
 
 
